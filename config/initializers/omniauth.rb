cb800360df098533b42314663f1eeaf64573fbed
ganondalf <cganon@gmail.com>
2014-05-11 12:57:03 -0400

    edits

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,3 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'] , ENV['GOOGLE_CLIENT_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
-
-# '849314944963-rv5s2rvju5kjlkhuso2tk20lj8h2h3kf.apps.googleusercontent.com'
-
-# 'pz30UdjXfeskWUoAQ5DEsIg_'
