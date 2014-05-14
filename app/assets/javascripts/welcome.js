cc8fd63c66aab9683215380674b83f56312f078b
ganondalf <cganon@gmail.com>
2014-05-14 03:18:28 -0400

    login

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,13 +2,20 @@
 var welcomeIndex = {
   onReady: function(){
     $("#newUserFormDiv").hide();
+    $("#returningUserFormDiv").hide();
+    $("#signup_button").click(welcomeIndex.showUserSignup);
     $("#login_button").click(welcomeIndex.showUserLogin);
     $('input[value="Create User"]').on("click", welcomeIndex.userLogin);
   },
 
-  showUserLogin: function(event){
+  showUserSignup: function(event){
       event.preventDefault();
       $("#newUserFormDiv").slideDown();
+  },
+
+  showUserLogin: function(event){
+      event.preventDefault();
+      $("#returningUserFormDiv").slideDown();
   },
 
   userLogin: function(event){
