319508e2babd9feb7eea91b4905837b075f2be79
ganondalf <cganon@gmail.com>
2014-05-15 21:08:56 -0400

    asdfasa

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,8 +4,7 @@ var welcomeIndex = {
     $(".returningUserFormDiv").hide();
     $("#signup_button").click(welcomeIndex.showUserSignup);
     $("#login_button").click(welcomeIndex.showUserLogin);
-    // $('input[value="Create User"]').on("click", welcomeIndex.createAccount);
-    // $('input[value="Login"]').on("click", welcomeIndex.userLogin);
+
   },
 
   showUserSignup: function(event){
--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -3,7 +3,7 @@ class Annotation < ActiveRecord::Base
     belongs_to :user
 
     def self.first_default
-       annotation = Annotation.create(body: "Click on this link to begin setting up your LinkLibrary collections and begin organizing your saved urls!")
+      annotation = Annotation.create(body: "Click on this link to begin setting up your LinkLibrary collections and begin organizing your saved urls!")
       return annotation
     end
 end
